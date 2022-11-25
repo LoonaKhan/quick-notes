500abb0d083b77ab61ab6d2af3b00bbae3b8a330
yourusername <114538241+Fosrock21@users.noreply.github.com>
2022-11-25 00:18:51 -0500

    working login and signup

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,6 +1,10 @@
-// window.onload = function () {
-//     ajaxCall('noteFolder.json', "GET", processFolderNotes);
-// }
+window.onload = function () {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const userId = urlParams.get('id');
+    let url = "http://localhost:4000/api/folders/user/" + userId;
+    ajaxCall(url, "GET", processFolderNotes);
+}
 
 function placeContentInMainBody(html) {
     document.getElementById("main-body").innerHTML = html;
@@ -13,7 +17,7 @@ function ajaxCall(url, action, callback, post = null) {
     xmlhttp.send(post);
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState === 4) {
-            if (xmlhttp.status === 200) {
+            if (xmlhttp.status === 200 || xmlhttp.status === 201) {
                 callback(xmlhttp.responseText);
             }
             else
@@ -53,22 +57,24 @@ function onLogin() {
         username: username, password: password
     });
 
-    ajaxCall(url, "POST", redirect, post);
+    ajaxCall(url, "POST", redirectToMainPage, post);
 
 }
 
-function redirect(url) {
-    window.location.replace(url)
+function redirectToMainPage(user) {
+    window.location.replace("/index");
+
+    let url = "http://localhost:4000/api/folders/" + user.id;
+    ajaxCall(url, "GET", processFolderNotes, post);
 }
 
 function processFolderNotes(response) {
     let html = "<div class='container-fluid'>";
+    let folders = JSON.parse(response);
 
-    let data_arr = JSON.parse(response);
+    for (let i = 0; i < folders.length; i++) {
 
-    for (let i = 0; i < data_arr.length; i++) {
-
-        var folderObj = data_arr[i];
+        var folderObj = folders[i];
         var folderJson = JSON.stringify(folderObj);
 
         if ((i % 3) == 0)
@@ -77,7 +83,7 @@ function processFolderNotes(response) {
 
         // Printing Folders
         html += "<div class='col-4'><div class='ml-1 p-2 roundeda' style='background: #808080; height: 220px; '>";
-        html += "<div class='rounded-pill p-2 m-1' style='background: #C4C4C4;'>" + folderObj.Folder;
+        html += "<div class='rounded-pill p-2 m-1' style='background: #C4C4C4;'>" + folderObj.name;
         html += "<div class='float-right'>";
         html += "<button type='button' class='btn p-0 pr-1'><i class='fa fa-star'></i></button>";
         html += "<button type='button' class='btn p-0 pr-1' onClick='onEditNotes(" + folderJson + ")'><i class='fa fa-pencil-square-o'></i></button>";
@@ -85,25 +91,25 @@ function processFolderNotes(response) {
         html += "</div>";
 
         // Printing Notes in folders
-        let isRowNoteClosed;
-        html += "<div class='container-fluid'>";
-        for (let j = 0; j < folderObj.Notes.length; j++) {
-            if ((j % 4) == 0) {
-                html += "<div class='row mt-3 mb-3'>";
-                isRowNoteClosed = false;
-            }
 
-            html += "<div class='col-3'><div style='background: #D9D9D9; height: 50px;'></div></div>";
+        //let isRowNoteClosed;
+        //html += "<div class='container-fluid'>";
+        // for (let j = 0; j < folderObj.Notes.length; j++) {
+        //     if ((j % 4) == 0) {
+        //         html += "<div class='row mt-3 mb-3'>";
+        //         isRowNoteClosed = false;
+        //     }
 
-            if (((j + 1) % 4) == 0) {
-                html += "</div>"; //This is the notes row div
-                isRowNoteClosed = true;
-            }
-            if (j == 7) break;
-        }
-        if (!isRowNoteClosed) html += "</div>"; //This is the final notes row div
-        html += "</div>"; //This is the note container div
+        //     html += "<div class='col-3'><div style='background: #D9D9D9; height: 50px;'></div></div>";
 
+        //     if (((j + 1) % 4) == 0) {
+        //         html += "</div>"; //This is the notes row div
+        //         isRowNoteClosed = true;
+        //     }
+        //     if (j == 7) break;
+        // }
+        //if (!isRowNoteClosed) html += "</div>"; //This is the final notes row div
+        //html += "</div>"; //This is the note container div
 
         //Closes the folder div
         html += "</div></div>";
---
+++ src/public/js/login.js
@@ -0,0 +1,47 @@
+function ajaxCall(url, action, callback, post = null) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open(action, url, true);
+    xmlhttp.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+    xmlhttp.send(post);
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4) {
+            if (xmlhttp.status === 200 || xmlhttp.status === 201) {
+                callback(xmlhttp.responseText);
+            }
+            else
+                callback("Error \n" + xmlhttp.responseText);
+        }
+    }
+}
+
+function onCreateUser() {
+    let username = document.getElementById("Username").value;
+    let password = document.getElementById("Password").value;
+
+    let url = "http://localhost:4000/api/users/create";
+    let post = JSON.stringify({
+        username: username, password: password, dark_mode: true,
+        avatar: 1,
+    });
+
+    ajaxCall(url, "POST", alert, post);
+}
+
+function onLogin() {
+    let username = document.getElementById("Username").value;
+    let password = document.getElementById("Password").value;
+
+    let url = "http://localhost:4000/api/users/login";
+    let post = JSON.stringify({
+        username: username, password: password
+    });
+
+    ajaxCall(url, "POST", redirectToMainPage, post);
+
+}
+
+function redirectToMainPage(response) {
+    let user = JSON.parse(response);
+    let url = "/index?id=" + user.id;
+    window.location.replace(url);
+}
